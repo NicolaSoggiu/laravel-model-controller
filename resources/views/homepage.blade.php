f6ae1ca5d7d582b5faa2fff1f22346ad6a1dd000
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-26 15:33:02 +0200

    add style

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,16 +2,18 @@
 
 @section("contents")
 
+<h2 class="text-center text-danger p-5"> Movies List : </h2>
+
 <div class="row g-3 row-cols-3">
     @foreach($collMovies as $movie)
         <div class="col">
             <div class="card h-100">
                 <div class="card-body">
-                <h5 class="card-title">{{ $movie->title}}</h5>
-                <h5 class="card-title">{{ $movie->orginal_title}}</h5>
-                <h5 class="card-title">{{ $movie->nationality}}</h5>
-                <h5 class="card-title">{{ $movie->date}}</h5>
-                <h5 class="card-title">{{ $movie->vote}}</h5>
+                <h5 class="card-title"> Title : {{ $movie->title}}</h5>
+                <h5 class="card-title"> Original Title : {{ $movie->orginal_title}}</h5>
+                <h5 class="card-title"> Language : {{ $movie->nationality}}</h5>
+                <h5 class="card-title"> Release date : {{ $movie->date}}</h5>
+                <h5 class="card-title"> Vote : {{ $movie->vote}}</h5>
                 </div>
             </div>
         </div>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,9 +12,9 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </head>
 
-<body>
+<body class="bg-dark">
 
-    <div class="container">
+    <div class="container p-3">
         <main>
             @yield("contents")
         </main>
