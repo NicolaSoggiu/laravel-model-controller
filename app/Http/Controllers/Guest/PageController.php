bf712ebe39300f6d6a4c2e876a8a1c8758943691
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-26 15:20:09 +0200

    add films in view

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Guest;
 
-use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        $collMovies = Movie::all();
+        return view("homepage", compact("collMovies"));
+    }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,21 @@
 @extends('layouts.base')
+
+@section("contents")
+
+<div class="row g-3 row-cols-3">
+    @foreach($collMovies as $movie)
+        <div class="col">
+            <div class="card h-100">
+                <div class="card-body">
+                <h5 class="card-title">{{ $movie->title}}</h5>
+                <h5 class="card-title">{{ $movie->orginal_title}}</h5>
+                <h5 class="card-title">{{ $movie->nationality}}</h5>
+                <h5 class="card-title">{{ $movie->date}}</h5>
+                <h5 class="card-title">{{ $movie->vote}}</h5>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+
+@endsection
