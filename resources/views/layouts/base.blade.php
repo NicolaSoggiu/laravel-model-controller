3713c3de3e55d0336bb6e89cbd0768b009cccdb5
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-26 14:53:04 +0200

    add folder

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,20 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Dc Comic</title>
-        <link rel="icon" href="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" type="image/x-icon">
+        <title>Moovie</title>
         <link
         href="https://fonts.googleapis.com/css2?family=Bangers&display=swap"
         rel="stylesheet"
         />
-        @vite('resources/js/app.js')
     </head>
 
 <body>
-    @include("partials.header")
-    
-    @include("partials.main")
-
-    @include("partials.footer")
 </body>
 </html>
